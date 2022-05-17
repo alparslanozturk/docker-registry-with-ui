87bfb36ab1ea3dca9e3c9ccaed820e8a634da7e3
Alparslan <alparslan.ozturk@gmail.com>
2022-05-17 11:15:56 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 
 
-# registry/config.yml dosyasi buradaki env ile degistirilir. 
-# htpasswd -b -B -c auth/htpasswd  dockeruser dockerpass
-#
-# /etc/docker/daemon.json
-#  "registry-mirrors": ["http://registry:5000"],
-#  "insecure-registries": ["registry:5000"],
-#
-
-#openssl genrsa -out certs/registry.key 4096
-#openssl req -new -x509 -text -days 3650 -subj "/C=TR/ST=TR/CN=registry" -key certs/registry.key -out certs/registry.crt
-
-# openssl req -x509 -days 3650 --nodes -subj "/C=TR/ST=TR/CN=registry" -out certs/registry.crt -keyout certs/registry.key
-#
-# sudo cp certs/registry.crt  /etc/ssl/registry.crt
-# sudo update-ca-certificates
-# curl -> /etc/ssl/certs/ca-certificates.crt doyasini kullanir...
-#
-# sudo cp certs/registry.crt  /usr/share/ca-certificates/   
-# sudo update-ca-certificates
-#
-#
-# add /etc/hosts    "127.0.0.1 registry"
-#
-#
-# test: curl -vvv -u "dockeruser:dockerpass" -isk https://registry/v2/_catalog
+### test: curl -vvv -u "dockeruser:dockerpass" -isk https://registry/v2/_catalog
 
 
 docker rm registry --force 
